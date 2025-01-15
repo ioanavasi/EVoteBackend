7ff0914501279666e3878999e7f593d0546e38ca
Ioana Vasile <ioanaaa140@gmail.com>
2025-01-15 03:10:56 +0200

    New tables and routes

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, VARCHAR
+from sqlalchemy import Integer, VARCHAR, ForeignKey, BigInteger, Text, UniqueConstraint
 from sqlalchemy.orm import mapped_column
 from flask_login import UserMixin
 from app import db
@@ -10,8 +10,8 @@ class User(db.Model, UserMixin):
     cnp = mapped_column(VARCHAR(13), unique=True)
     email = mapped_column(VARCHAR(255), unique=True)
     hashed_password = mapped_column(VARCHAR(255))
-    first_name = mapped_column(VARCHAR(255), nullable=True)
-    last_name = mapped_column(VARCHAR(255), nullable=True)
+    first_name = mapped_column(VARCHAR(255))
+    last_name = mapped_column(VARCHAR(255))
 
     def to_dict(self):
         return {
@@ -21,6 +21,40 @@ class User(db.Model, UserMixin):
             'fist_name': self.first_name,
             'last_name': self.last_name
         }
+    
+class Candidate(db.Model):
+    id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    election_id = mapped_column(Integer, ForeignKey('election.id'))
+    name = mapped_column(VARCHAR(255))
+    party = mapped_column(VARCHAR(255))
+    slogan = mapped_column(Text, nullable=True)
 
-    def __repr__(self):
-        return f'<User {self.cnp}>'
+    __table_args__ = (
+        UniqueConstraint('name', 'election_id', name='unique_candidate_election'),
+    )
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'election_id': self.election_id,
+            'name': self.name,
+            'party': self.party,
+            'slogan': self.slogan
+        }
+
+class Election(db.Model):
+    id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name = mapped_column(VARCHAR(255), unique=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+class Vote(db.Model):
+    id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user_uuid = mapped_column(VARCHAR(36), ForeignKey('user.uuid'))
+    candidate_id = mapped_column(Integer, ForeignKey('candidate.id'))
+    election_id = mapped_column(Integer, ForeignKey('election.id'))
+    timestamp = mapped_column(BigInteger)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from app.models import User
+from app.models import User, Candidate, Election, Vote
 from app import db, login_manager
 
 routes_bp = Blueprint('routes', __name__)
@@ -27,9 +27,9 @@ def register():
     data = request.json
 
     try:
-        existing_user = User.query.filter_by(email=data['email']).first()
+        existing_user = User.query.filter_by(cnp=data['cnp']).first()
         if existing_user:
-            return jsonify("Email already in use"), 400
+            return jsonify("CNP already in use"), 400
 
         new_user = User(
             email=data['email'],
@@ -73,3 +73,182 @@ def logout():
         return jsonify("Logout successful"), 200
     except Exception as e:
         return jsonify(str(e)), 400
+    
+
+@routes_bp.route('/vote', methods=['POST'])
+@login_required
+def vote():
+    data = request.json
+    try:
+        election = Election.query.filter_by(id=data['election_id']).first()
+        if election is None:
+            return jsonify("Inexistent election"), 400
+
+        candidate = Candidate.query.filter_by(id=data['candidate_id']).filter_by(election_id=election.id).first()
+        if candidate is None:
+            return jsonify("Inexistent candidate"), 400
+
+        existing_vote = Vote.query.filter_by(user_uuid=current_user.uuid).filter_by(election_id=election.id).first()
+        if existing_vote:
+            return jsonify("User already voted in this election"), 400
+
+        new_vote = Vote(
+            user_uuid = current_user.uuid,
+            candidate_id = candidate.id,
+            election_id = election.id,
+            timestamp = data['timestamp']
+        )
+
+        db.session.add(new_vote)
+        db.session.commit()
+
+        return jsonify("Vote successful"), 201
+    except Exception as e:
+        return jsonify(str(e)), 400
+
+@routes_bp.route('/vote/status/<int:e_id>', methods=['GET'])
+@login_required
+def get_voted_status(e_id):
+    try:
+        election = Election.query.filter_by(id=e_id).first()
+        if election is None:
+            return jsonify("Inexistent election"), 400
+        vote = Vote.query.filter_by(user_uuid=current_user.uuid).filter_by(election_id=e_id).first()
+        vote_status = {}
+        if vote:
+            vote_status['status'] = 'YES'
+        else:
+            vote_status['status'] = 'NO'
+        return jsonify(vote_status), 200
+    except Exception as e:
+        return jsonify(str(e)), 500    
+        
+
+@routes_bp.route('/vote/history', methods=['GET'])
+@login_required
+def get_vote_history():
+    try:
+        votes = Vote.query.filter_by(user_uuid=current_user.uuid).all()
+
+        vote_history = []
+        for vote in votes:
+            election = Election.query.filter_by(id=vote.election_id).first()
+            candidate = Candidate.query.filter_by(id=vote.candidate_id).first()
+            vote_history_entry = {
+                'election_name': election.name,
+                'candidate_name': candidate.name,
+                'candidate_party': candidate.party,
+                'timestamp': vote.timestamp
+            }
+            vote_history.append(vote_history_entry)
+        
+        return jsonify(vote_history), 200
+    except Exception as e:
+        return jsonify(str(e)), 500
+    
+
+@routes_bp.route('/election', methods=['GET'])
+def get_elections():
+    try:
+        elections = Election.query.all()
+        return jsonify([election.to_dict() for election in elections]), 200
+    except Exception as e:
+        return jsonify(str(e)), 500
+    
+@routes_bp.route('/election', methods=['POST'])
+@login_required
+def add_election():
+    if current_user.cnp != "0000000000000":
+        return jsonify("User not authorized for this operation"), 401
+    
+    data = request.json
+    try:
+        existing_election = Election.query.filter_by(name=data['name']).first()
+        if existing_election:
+            return jsonify("An election with this name already exists"), 400
+        
+        new_election = Election(
+            name=data['name']
+        )
+
+        db.session.add(new_election)
+        db.session.commit()
+        return jsonify("Election added successfully"), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify(str(e)), 500
+    
+@routes_bp.route('/election/<int:e_id>', methods=['DELETE'])
+@login_required
+def delete_election(e_id):
+    if current_user.cnp != "0000000000000":
+        return jsonify("User not authorized for this operation"), 401
+    
+    try:
+        election = Election.query.filter_by(id=e_id).first()
+
+        if election is None:
+            return jsonify("Inexistent election"), 400
+        
+        db.session.delete(election)
+        db.session.commit()
+        
+        return jsonify("Election deleted successfully"), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify(str(e)), 500
+
+@routes_bp.route('/candidate/<int:e_id>', methods=['GET'])
+def get_candidates_by_election_id(e_id):
+    try:
+        candidates = Candidate.query.filter_by(election_id=e_id).all()
+        return jsonify([candidate.to_dict() for candidate in candidates]), 200
+    except Exception as e:
+        return jsonify(str(e)), 500
+    
+@routes_bp.route('/candidate', methods=['POST'])
+@login_required
+def add_candidate():
+    if current_user.cnp != "0000000000000":
+        return jsonify("User not authorized for this operation"), 401
+    
+    data = request.json
+    try:
+        existing_candidate = Candidate.query.filter_by(name=data['name']).first()
+        if existing_candidate:
+            return jsonify("A candidate with this name already exists"), 400
+        
+        new_candidate = Candidate(
+            election_id=data['election_id'],
+            name=data['name'],
+            party=data['party'],
+            slogan=data.get('slogan')
+        )
+
+        db.session.add(new_candidate)
+        db.session.commit()
+        
+        return jsonify("Candidate added successfully"), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify(str(e)), 500
+    
+@routes_bp.route('/candidate/<int:c_id>/<int:e_id>', methods=['DELETE'])
+@login_required
+def delete_candidate(c_id, e_id):
+    if current_user.cnp != "0000000000000":
+        return jsonify("User not authorized for this operation"), 401
+    
+    try:
+        candidate = Candidate.query.filter_by(id=c_id).filter_by(election_id=e_id)
+
+        if candidate is None:
+            return jsonify("Inexistent candidate"), 400
+        
+        db.session.delete(candidate)
+        db.session.commit()
+        
+        return jsonify("Candidate deleted successfully"), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify(str(e)), 500
